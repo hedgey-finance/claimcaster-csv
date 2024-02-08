93a0a9ab317db1ccdb6bd82753f34d0503ee2853
Shawn Simas <shawn@hedgey.finance>
2024-02-08 11:39:38 +0000

    Add option for random distributions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,25 @@
 
 	let channel = '';
 	let tokenAmount = 0;
+	let minAmount = 1;
+	let maxAmount = 100;
+
+	let randomDistribution = false;
 
 	const generateCSV = async () => {
 		loading = true;
 
 		if (!channel) {
 			error = 'Channel name is required';
+			loading = false;
+			return;
+		}
+
+		if (
+			randomDistribution &&
+			(!minAmount || !maxAmount || minAmount < 0 || maxAmount < 1 || minAmount > maxAmount)
+		) {
+			error = 'Invalid token range';
 			loading = false;
 			return;
 		}
@@ -39,7 +52,10 @@
 			}
 
 			for (const user of users) {
-				if (user.verifications) csvData.push(`${user.verifications[0]},${tokenAmount}`);
+				const userTokens = randomDistribution
+					? Math.floor(Math.random() * (maxAmount - minAmount + 1)) + minAmount
+					: tokenAmount;
+				if (user.verifications) csvData.push(`${user.verifications[0]},${userTokens}`);
 			}
 			if (csvData.length == 1) {
 				error = 'No users with addresses found in the channel';
@@ -69,7 +85,7 @@
 		<div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
 			<div class="mb-4">
 				<label class="block text-gray-700 text-sm font-bold mb-2">
-					Farcaster channel name:
+					Farcaster channel name
 					<input
 						class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
 						id="username"
@@ -78,17 +94,53 @@
 					/>
 				</label>
 			</div>
-			<div class="mb-4">
-				<label class="block text-gray-700 text-sm font-bold mb-2">
-					Tokens per user:
+
+			<div class="flex items-center mb-4">
+				<label class="text-sm text-gray-700 font-bold">
 					<input
-						class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-						id="username"
-						type="text"
-						bind:value={tokenAmount}
+						type="checkbox"
+						class="bg-gray-50 border-gray-300 focus:ring-3 focus:ring-blue-300 h-4 w-4 rounded"
+						bind:checked={randomDistribution}
 					/>
+					Use random distribution
 				</label>
 			</div>
+
+			{#if randomDistribution}
+				<div class="mb-4 flex gap-2">
+					<label class="block text-gray-700 text-sm font-bold mb-2">
+						Min tokens per user
+						<input
+							class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							id="username"
+							type="text"
+							bind:value={minAmount}
+						/>
+					</label>
+					<label class="block text-gray-700 text-sm font-bold mb-2">
+						Max tokens per user
+						<input
+							class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							id="username"
+							type="text"
+							bind:value={maxAmount}
+						/>
+					</label>
+				</div>
+			{:else}
+				<div class="mb-4">
+					<label class="block text-gray-700 text-sm font-bold mb-2">
+						Tokens per user
+						<input
+							class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							id="username"
+							type="text"
+							bind:value={tokenAmount}
+						/>
+					</label>
+				</div>
+			{/if}
+
 			<div class="text-center">
 				{#if loading}
 					<button
